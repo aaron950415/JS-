bef85cdab3f1f3b7199bf60e0793e3a4ffe1703f
Aaron <aaron950415@qq.com>
2020-07-15 23:43:15 +1000

    array min

---
+++ array-min.js
@@ -0,0 +1,19 @@
+let arr=[];
+for(let i=0; i<15;i++){
+  arr[i] = Math.round(Math.random()*100);
+}
+let min=(numbers)=>{
+  if(numbers.length > 2){
+    return min([numbers[0],min(numbers.slice(1))]);
+  }else{
+ if(numbers[0]>numbers[1]){
+      return numbers[1]>numbers[2] ? numbers[2] : numbers[1]
+    }else{
+      return numbers[0]>numbers[2] ? numbers[2] : numbers[0]
+    }
+
+  } 
+      
+};
+console.log(min(arr));
+console.log(arr);
