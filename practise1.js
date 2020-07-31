982e1c425286c081a163b93dfb68a3edff7a7980
aaron950415 <50894029+aaron950415@users.noreply.github.com>
2020-08-01 00:25:35 +1000

    Update practise1.js

--- practise1.js
+++ practise1.js
@@ -1,4 +1,4 @@
-let scores1 = [95,91,59,55,42,82,72,85,67,66,55,91]
+let scores1 = [95,91,59,55,42,82,72,85,67,66,55,91];
 let sum = scores1.reduce((sum, n)=> n%2===0 ? sum : sum+n,0)
 console.log(sum) // 奇数之和：598 
 
